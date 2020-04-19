918b9ff3f5df0ce482e552ce9e36ddc7a324a9e5
Emmanuel Oguntoyinbo <gramme.oe@gmail.com>
2020-04-19 23:48:16 +0100

    Converts Favorites to Local State Logic

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -9,10 +9,16 @@ enum FilterOptions {
   All,
 }
 
-class ProductsOverviewScreen extends StatelessWidget {
+class ProductsOverviewScreen extends StatefulWidget {
+  @override
+  _ProductsOverviewScreenState createState() => _ProductsOverviewScreenState();
+}
+
+class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
+  var _showFavoritesOnly = false;
+
   @override
   Widget build(BuildContext context) {
-    final productsContainer = Provider.of<Products>(context, listen: false);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Grammart'),
@@ -20,9 +26,9 @@ class ProductsOverviewScreen extends StatelessWidget {
           PopupMenuButton(
             onSelected: (FilterOptions selectedValue) {
               if (selectedValue == FilterOptions.Favorites) {
-                productsContainer.showFavoritesOnly();
+                _showFavoritesOnly = true;
               } else {
-                productsContainer.showAll();
+                _showFavoritesOnly = false;
               }
             },
             itemBuilder: (ctx) => [
@@ -35,7 +41,7 @@ class ProductsOverviewScreen extends StatelessWidget {
           )
         ],
       ),
-      body: ProductsGrid(),
+      body: ProductsGrid(_showFavoritesOnly),
     );
   }
 }
