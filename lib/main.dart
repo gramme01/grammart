e3b86671b955b2433d4034a0c20bdcdadc2695c1
Emmanuel Oguntoyinbo <gramme.oe@gmail.com>
2020-04-20 03:01:13 +0100

    Changes products_provider.dart to products.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:provider/provider.dart';
 
 import './screens/product_detail_screen.dart';
 import './screens/products_overview_screen.dart';
-import './providers/products_provider.dart';
+import './providers/products.dart';
 
 void main() => runApp(MyApp());
 
