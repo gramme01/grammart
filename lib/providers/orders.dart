b6afd580b8e94a8514d753d5e7c39701118cb725
Emmanuel Oguntoyinbo <gramme.oe@gmail.com>
2020-04-23 23:28:14 +0100

    Stores orders on the server

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
 
 import '../providers/cart.dart';
 
@@ -23,14 +26,34 @@ class Orders with ChangeNotifier {
     return [..._orders];
   }
 
-  void addOrder(List<CartItem> cartProducts, double total) {
+  static const String orderServer =
+      'https://flutter-shop-app-22b2b.firebaseio.com/orders';
+
+  Future<void> addOrder(List<CartItem> cartProducts, double total) async {
+    const url = '$orderServer.json';
+    final timestamp = DateTime.now();
+    final resp = await http.post(
+      url,
+      body: json.encode({
+        'amount': total,
+        'dateTime': timestamp.toIso8601String(),
+        'products': cartProducts
+            .map((cp) => {
+                  'id': cp.id,
+                  'title': cp.title,
+                  'quantity': cp.quantity,
+                  'price': cp.price,
+                })
+            .toList(),
+      }),
+    );
     _orders.insert(
       0,
       OrderItem(
-        id: DateTime.now().toString(),
+        id: json.decode(resp.body)['name'],
         amount: total,
         products: cartProducts,
-        dateTime: DateTime.now(),
+        dateTime: timestamp,
       ),
     );
     notifyListeners();
