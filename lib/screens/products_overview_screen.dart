3fb5714859e53d566b010cfc580ab2749327b587
Emmanuel Oguntoyinbo <gramme.oe@gmail.com>
2020-04-23 04:39:16 +0100

    Connect Product grid to server fetched products

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -23,6 +23,7 @@ class ProductsOverviewScreen extends StatefulWidget {
 class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
   var _showFavoritesOnly = false;
   var _isInit = true;
+  var _isLoading = false;
 
   @override
   void initState() {
@@ -38,7 +39,17 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
 
   @override
   void didChangeDependencies() {
-    if (_isInit) Provider.of<Products>(context).fetchAndSetProducts();
+    if (_isInit) {
+      setState(() {
+        _isLoading = true;
+      });
+      Provider.of<Products>(context).fetchAndSetProducts().then((_) {
+        setState(() {
+          _isLoading = false;
+        });
+      });
+    }
+    ;
     _isInit = false;
     super.didChangeDependencies();
   }
@@ -84,7 +95,11 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
         ],
       ),
       drawer: AppDrawer(),
-      body: ProductsGrid(_showFavoritesOnly),
+      body: _isLoading
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : ProductsGrid(_showFavoritesOnly),
     );
   }
 }
