b1bbedd7a8b7d216651fa6358285cf0caa3f9e38
Emmanuel Oguntoyinbo <gramme.oe@gmail.com>
2020-04-26 16:19:07 +0100

    Adds logout functionality

--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:grammart/providers/auth.dart';
+import 'package:provider/provider.dart';
 
 import '../screens/user_products_screen.dart';
 import '../screens/orders_screen.dart';
@@ -20,6 +22,7 @@ class AppDrawer extends StatelessWidget {
             Navigator.of(context).pushReplacementNamed('/');
           },
         ),
+        Divider(),
         ListTile(
           leading: Icon(Icons.payment),
           title: Text('Orders'),
@@ -27,12 +30,22 @@ class AppDrawer extends StatelessWidget {
             Navigator.of(context).pushReplacementNamed(OrdersScreen.routeName);
           },
         ),
+        Divider(),
         ListTile(
           leading: Icon(Icons.edit),
           title: Text('Manage Products'),
           onTap: () {
             Navigator.of(context)
                 .pushReplacementNamed(UserProductsScreen.routeName);
+          },
+        ),
+        Divider(),
+        ListTile(
+          leading: Icon(Icons.exit_to_app),
+          title: Text('Logout'),
+          onTap: () {
+            Navigator.of(context).pop();
+            Provider.of<Auth>(context, listen: false).logout();
           },
         ),
       ]),
