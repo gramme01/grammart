62c50fde3d4146d2ee821945b79189c1f5eab260
Emmanuel Oguntoyinbo <gramme.oe@gmail.com>
2020-04-27 21:23:32 +0100

    Adds Hero transition to product details

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -29,10 +29,14 @@ class ProductItem extends StatelessWidget {
               Navigator.of(context).pushNamed(ProductDetailScreen.routeName,
                   arguments: product.id);
             },
-            child: FadeInImage(
-              placeholder: AssetImage('assets/images/product-placeholder.jpg'),
-              image: NetworkImage(product.imageUrl),
-              fit: BoxFit.cover,
+            child: Hero(
+              tag: product.id,
+              child: FadeInImage(
+                placeholder:
+                    AssetImage('assets/images/product-placeholder.jpg'),
+                image: NetworkImage(product.imageUrl),
+                fit: BoxFit.cover,
+              ),
             )),
         footer: GridTileBar(
           backgroundColor: Colors.black38,
